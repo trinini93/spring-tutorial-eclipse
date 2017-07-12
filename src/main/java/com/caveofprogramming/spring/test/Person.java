305a17305ca21254c6b94b6d6c39b7bf6c923b88
trinini93 <trina.ch93@gmail.com>
2017-07-12 17:58:05 -0400

    mintest

--- src/main/java/com/caveofprogramming/spring/test/Person.java
+++ src/main/java/com/caveofprogramming/spring/test/Person.java
@@ -3,7 +3,7 @@ package com.caveofprogramming.spring.test;
 public class Person {
 	
 	public void speak () {
-		System.out.println("Hello, I'm a person! ");
+		System.out.println("Hello, I'm a person. ");
 	}
 
 }
